a3b8c2e701ead86a1a4bb42b4423e22baf012e57
Tom Bloor <tom@tbsliver.me>
2018-06-20 01:33:41 +0100

    Re-doing layout slightly

---
+++ graphics/elements/tpcig-omnibar/tpcig-omnibar-social.js
@@ -0,0 +1,5 @@
+class TpcigOmnibarSocial extends Polymer.Element {
+  static get is() { return "tpcig-omnibar-social" }
+  constructor() { super() }
+}
+customElements.define(TpcigOmnibarSocial.is, TpcigOmnibarSocial);
